ff972cb6a40edbb81154bb8fa22107c58338f8a3
/elchopo7 <elchopo7@gmail.com>
2026-04-29 22:19:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const express = require("express");
+
+// Vercel looks for an entrypoint that imports Express directly.
+// We keep the real app in server/app.js and load it here for deployment.
 module.exports = require("./server/app");
